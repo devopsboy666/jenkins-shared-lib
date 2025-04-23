f6804be9b95713c00fa4e6c915c00ffaff4bbf27
devopsboy666 <pakawatpenpoom@gmail.com>
2025-04-23 10:10:03 +0700

    edit class CloneRepo

--- src/com/demo/CloneRepo.groovy
+++ src/com/demo/CloneRepo.groovy
@@ -3,13 +3,27 @@ package com.demo
 class CloneRepo implements Serializable {
     
     def steps
+    def branch 
+    def repoUrl
 
-    CloneRepo(steps) {
-        this.steps = steps
+    // Constructor 
+    CloneRepo(Map args) {
+        if (!args.steps) throw new IllegalArgumentException("Missing required argument: steps")
+        this.steps = args.steps
+        this.branch = args.branch :? 'main'
+        this.repoUrl = args.repoUrl :? ''
+
+        if (this.repoUrl == '') {
+            error("Repo URL is empty")
+        }
     }
 
-    def sayHello(String message) {
-        steps.echo "Hello ${message}"
+    gitCloneRepo() {
+        try {
+            steps.sh "git clone -b ${this.branch} ${this.repoUrl}"
+        } catch (err) {
+            error("Git Clone Fail..\n${err.message}")
+        }
     }
 
 }
