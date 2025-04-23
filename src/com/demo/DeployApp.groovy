fd5bfa1312edbc0fe325f3d10d0b10b1bf99d24b
devopsboy666 <pakawatpenpoom@gmail.com>
2025-04-23 17:00:07 +0700

    Edit Class Deploy method helmDeploy

--- src/com/demo/DeployApp.groovy
+++ src/com/demo/DeployApp.groovy
@@ -25,7 +25,7 @@ class DeployApp implements Serializable {
 
     def helmDeploy() {
         try {
-            steps.sh "helm upgrade --install ${this.releaseName} ${this.pathHelm} . --set image.name=${this.imageName} --set image.tag=${this.imageTag}"
+            steps.sh "helm upgrade --install ${this.releaseName} ${this.pathHelm} --set image.name=${this.imageName} --set image.tag=${this.imageTag}"
         } catch (err) {
             steps.error("Fail Helm Deploy ${err.message}")
         }
